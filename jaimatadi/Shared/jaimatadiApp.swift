6ab913c8b381c6ef581530a10980e45eaa856d25
Amarendra baitha <kumaramarendra555@gmail.com>
2022-01-16 14:16:36 +0530

    Sift project

---
+++ jaimatadi/Shared/jaimatadiApp.swift
@@ -0,0 +1,17 @@
+//
+//  jaimatadiApp.swift
+//  Shared
+//
+//  Created by Amarendra on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct jaimatadiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
